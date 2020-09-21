aa60be2250f6fb20eab6a27719e2897137d55c8a
Oleg Kirichenko <oleg4564@me.com>
2020-09-21 13:05:06 -0400

    Update Package.swift
    making package dynamic

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Cosmos",
     products: [
-        .library(name: "Cosmos", targets: ["Cosmos"]),
+        .library(name: "Cosmos", type: .dynamic, targets: ["Cosmos"]),
     ],
     targets: [
         .target(name: "Cosmos", path: "./Cosmos"),
